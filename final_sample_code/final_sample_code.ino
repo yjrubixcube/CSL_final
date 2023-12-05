bf1ec2d7f75c6beeda5d8ef480c59bf9a2e72061
yjrubixcube <jyunwangt99@gmail.com>
2023-12-05 23:41:38 +0800

    first

---
+++ final_sample_code/final_sample_code.ino
@@ -0,0 +1,77 @@
+/***************************************************************/
+/* Sample code for CSL final project                           */
+/* Demonstrating 1) IR sensor, 2) Servo motor, and 3) DC Motor */
+/***************************************************************/
+
+// All grounds (GND) should be connected
+// After finishing prototyping, you can power Nano by connecting a 9V battery to Vin pin 
+
+/***** IR Sensor *****/
+#define ir_sensor A2 // A0 pin on the sensor
+// Vcc -> 5V
+// GND -> GND
+
+
+/***** Servo Motor *****/
+#define servo_pin 11 // PWM pin (orange)
+// Red -> 5V
+// Brown -> GND
+
+#include <Servo.h>
+Servo myservo;
+int servo_output = 0;
+
+
+/***** DC Motor (L298N) *****/
+#define ENA 5 // (pwm)
+#define IN1 6
+#define IN2 7
+// +12V -> (+) of 9V battery
+// GND -> GND
+// OUT1 & OUT2 -> DC Motor
+
+int dc_dir = 0;
+int dc_output = 0;
+
+
+void setup() {
+  /****** IR Sensor ******/
+  pinMode(ir_sensor, INPUT);
+
+  /***** Servo Motor *****/
+  pinMode(servo_pin, OUTPUT);
+  myservo.attach(servo_pin);
+
+  /** DC Motor (L298N) ***/
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  setDirection(dc_dir);
+  analogWrite(ENA, dc_output);
+  
+  Serial.begin(38400);
+}
+
+void loop() {
+  /* Read from the IR Sensor */
+  Serial.println(analogRead(ir_sensor));
+
+  /* Rotate the servo motor (by degree) */
+  myservo.write((servo_output += 10) % 180);
+
+  /* Set DC motor direction and power */
+  setDirection(dc_dir);
+  analogWrite(ENA, (dc_output += 63) % 255);
+  
+  delay(500);
+}
+
+void setDirection(int dir){
+  if (dir == 0){
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+  } else if (dir == 1){
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+  }
+}
