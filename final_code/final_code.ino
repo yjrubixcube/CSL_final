74515702bfe250da6b47482158e60e9cf25ff77b
yjrubixcube <jyunwangt99@gmail.com>
2023-12-13 23:41:06 +0800

    final

--- final_code/final_code.ino
+++ final_code/final_code.ino
@@ -17,6 +17,10 @@
 // GND -> GND
 
 
+#define START_MOTOR 255 // starting motor speed
+#define STOP_MOTOR 150 // reverse motor speed
+#define MAX_MOTOR_SPEED 200 // max motor speed for constant speed
+
 /***** Servo Motor *****/
 #define servo_pin 11 // PWM pin (orange)
 // Red -> 5V
@@ -42,6 +46,8 @@ int MID_SERVO = 75;
 
 int dc_dir = 0;
 int dc_output = 0;
+
+int servo_angle = 0;
 
 
 void setup() {
@@ -77,7 +83,7 @@ void loop() {
   Serial.print(analogRead(ir_sensor3));
   Serial.print("\n");
 
-  
+	servo_angle = map(value, from min, from max, MIN_SERVO, MAXSERVO);
 
   /* Rotate the servo motor (by degree) */
   // myservo.write((servo_output += 10) % 180);
@@ -99,15 +105,14 @@ void loop() {
 //   }
 
   /* Set DC motor direction and power */
-  setDirection(dc_dir);
-  analogWrite(ENA, 255);
-  
-  delay(1000);
-  analogWrite(ENA, 125);
-  delay(1000);
-  setDirection(1);
-  analogWrite(ENA, 100);
-  delay(750);
+	// start();
+	// delay(1000);
+	// analogWrite(ENA, MAX_MOTOR_SPEED);
+	// delay(1000);
+	if (count_blocks == 10){
+		reverse_and_stop();
+		start();
+	}
 }
 
 void setDirection(int dir){
@@ -118,4 +123,29 @@ void setDirection(int dir){
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
   }
+}
+
+
+void reverse_and_stop(int dir){
+	setDirection(1); // reverse motor
+	analogWrite(ENA, STOP_MOTOR);
+	delay(3000); // wait for 3 seconds
+}
+
+void start(){
+	setDirection(0);
+	analogWrite(ENA, START_MOTOR); // forward motor
+}
+
+float follow_line(){
+	float angle = 0; // (-10, 10)
+	return angle;
+}
+
+int count_blocks(){
+	return 10;
+}
+
+void handle_start(){
+
 }
